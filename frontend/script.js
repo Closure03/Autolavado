f7e755d7d4c32a3dcf86843a63636451f00ec9cb
Closure03 <88551025+Closure03@users.noreply.github.com>
2026-03-26 18:13:16 -0500

    updtv3

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,7 @@ const API_BASE = "https://autolavado-api-hka0hhbcfygjawey.canadacentral-01.azure
 
 // ── ESTADO ───────────────────────────────────────────────────────
 let editandoVehiculoId = null;
-let vehiculosCache     = [];   // para el select de servicios y la tablaaaaaa
+let vehiculosCache     = [];   // para el select de servicios y la tabla
 
 // ── PRECIOS SUGERIDOS ────────────────────────────────────────────
 const PRECIOS = {
